83eb0ee8272d707cad002b8f7f506b9d04a6d368
NICOLLEUNI <nicollemontano@unicauca.edu.co>
2025-10-12 23:37:10 -0500

    Initial commit

---
+++ platziPlay/src/main/java/com/platzi/play/PlatziPlayAiService.java
@@ -0,0 +1,15 @@
+package com.platzi.play;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface PlatziPlayAiService {
+
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforna de Gestion de Peliculas {{plataform}}.
+            Usa menos de 120 caracteres y hazlo con el estilo Platzi.
+            """)
+    String generateGreeting(@V("plataform") String plataform);
+}
